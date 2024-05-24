f80ffc4dec5ee95cd8b0f695b4f6341417ae8e7d
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-23 23:44:07 -0600

    add header and child image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import headerImage from './assets/header.png'
 import './App.css'
 
 function App() {
@@ -8,9 +8,11 @@ function App() {
 
   return (
     <>
+    <header className='header'>
+      <img src={headerImage} className='header__image' alt="Header background image" />
+    </header>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
