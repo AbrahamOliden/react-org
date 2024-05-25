f314c1c566b2496d9ea5e6b86c47b4ecf0ccae8c
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-24 22:25:04 -0600

    delete default vite code

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,7 @@ function App() {
   return (
     <>
       <Header />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Hello World with React!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
