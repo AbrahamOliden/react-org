ba702512a1d2a44af6039cb277ae075cb796dea1
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-26 17:23:17 -0600

    add preventDefault to form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,19 +4,23 @@ import DropDownMenu from '../dropdown/Dropdown.jsx';
 import Button from '../button/Button.jsx';
 
 function Form() {
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(e);
+    };
+
     return (
-        <>
-            <section className='form-container'>
-                <form className='form' action="">
-                    <h2 className='form__header title'>Fill the form to create a new collaborator card.</h2>
-                    <TextInput title='Name' placeholder='Introduce name' />
-                    <TextInput title='Position' placeholder='Introduce position'/>
-                    <TextInput title='Photo' placeholder='Introduce photo link' />
-                    <DropDownMenu title='Team' />
-                    <Button text='Create' />
-                </form>
-            </section>
-        </>
+        <section className='form-container'>
+            <form className='form' onSubmit={handleClick}>
+                <h2 className='form__header title'>Fill the form to create a new collaborator card.</h2>
+                <TextInput title='Name' placeholder='Introduce name' />
+                <TextInput title='Position' placeholder='Introduce position'/>
+                <TextInput title='Photo' placeholder='Introduce photo link' />
+                <DropDownMenu title='Team' />
+                <Button text='Create' />
+            </form>
+        </section>
     );
 };
 
