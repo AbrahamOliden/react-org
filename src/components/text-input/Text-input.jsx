0f599159e536bebf1b180d1739bb7c2c56582244
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-25 19:26:20 -0600

    access props values

--- src/components/text-input/Text-input.jsx
+++ src/components/text-input/Text-input.jsx
@@ -1,10 +1,10 @@
 import './Text-input.css';
 
-function TextInput() {
+function TextInput(props) {
     return (
         <div className='input-container' role='none'>
-            <label className='input-container__label'>Label</label>
-            <input className='input-container__input' type="text" placeholder='Introduce'/>
+            <label className='input-container__label'>{props.title}</label>
+            <input className='input-container__input' type="text" placeholder={props.placeholder} />
         </div>
     );
 };
