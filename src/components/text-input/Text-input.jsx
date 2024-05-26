014bbdcd29d5e5dadecfc1e22f09e09794945061
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-25 20:15:36 -0600

    link label and inputs with values set to props.title

--- src/components/text-input/Text-input.jsx
+++ src/components/text-input/Text-input.jsx
@@ -3,8 +3,8 @@ import './Text-input.css';
 function TextInput(props) {
     return (
         <div className='input-container' role='none'>
-            <label className='input-container__label'>{props.title}</label>
-            <input className='input-container__input' type="text" placeholder={props.placeholder} />
+            <label htmlFor={props.title} className='input-container__label'>{props.title}</label>
+            <input className='input-container__input' type="text" placeholder={props.placeholder} name={props.title} id={props.title}/>
         </div>
     );
 };
