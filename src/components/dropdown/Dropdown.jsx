e33c1ecb9c80bdb1093cb72de7641e2722ce8491
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-25 20:47:53 -0600

    add map function to create option element per team

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,14 +1,26 @@
-function DropDownMenu() {
+import './Dropdown.css'
+
+function DropDownMenu(props) {
+
+    const teams = [
+        "Programming",
+        "Front End",
+        "Data Science",
+        "Devops",
+        "UX/UI",
+        "Mobile",
+        "Innovation Management"
+    ];
+// <option value={team}>{team}</option>
     return (
         <div className="input-container" role="none">
-            <select name="team" >
-                <option value="">Programming</option>
-                <option value="">Front End</option>
-                <option value="">Data Science</option>
-                <option value="">Devops</option>
-                <option value="">UX/UI</option>
-                <option value="">Mobile</option>
-                <option value="">Innovation Management</option>
+            <label htmlFor="team" className="input-container__label">{props.title}</label>
+            <select name="team" id="team" className="dropdown">
+                { teams.map( (team) => {
+                    return (
+                        <option value={team}>{team}</option>
+                    );
+                } ) }
             </select>
         </div>
     )
