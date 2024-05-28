4192c75df8af85edb0cf55d54fdb7059983cde00
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-27 19:26:06 -0600

    create funciton handleClick function to use changeDisplay, and trigger it at button's onClick

--- src/components/my-org/my-org.jsx
+++ src/components/my-org/my-org.jsx
@@ -1,12 +1,17 @@
 import './my-org.css'
 import Collapse from '../../assets/collapse.png'
 
-function MyOrg() {
+function MyOrg(props) {
+
+    const handleClick = () => {
+        props.changeDisplay();
+    };
+
     return (
         <section className='org'>
             <div role='none' className='org__header' >
                 <h2 className='org__header__title title'>My Organization</h2>
-                <button className='org__header__button'>
+                <button onClick={handleClick} className='org__header__button'>
                     <img src={Collapse} alt='Collapse/Expand icon' ></img>
                 </button>
             </div>
