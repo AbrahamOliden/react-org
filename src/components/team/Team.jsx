96e6d6c7ccb6c89548c4ddddfaf4d7de703af830
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-28 17:12:38 -0600

    destructure props.data and use variables to declare dynamic styles

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -1,9 +1,12 @@
 import './Team.css'
 
 function Team(props) {
+
+    const {team, mainColor, accentColor} = props.data;
+
     return (
-        <section className='team' >
-            <h3 className='team__header title' >{props.team}</h3>
+        <section className='team' style={{backgroundColor: mainColor}} >
+            <h3 className='team__header title' style={{borderColor: accentColor}} >{team}</h3>
             <div className="collaborators"></div>
         </section>
     );
