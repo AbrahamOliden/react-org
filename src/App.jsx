3733864f3817791f1fe3b4b5a45b3830bd382cf9
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-28 15:37:38 -0600

    add Team component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from './components/header/Header.jsx';
 import Form from './components/form/Form.jsx';
 import MyOrg from './components/my-org/my-org.jsx';
+import Team from './components/team/Team.jsx';
 import './App.css';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Header />
       { display && <Form />}
       <MyOrg changeDisplay={changeDisplay} />
+      <Team />
     </>
   );
 };
