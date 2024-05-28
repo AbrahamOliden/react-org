3af241387ea95c35499969d44ad2543e23fd7e89
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-27 19:55:09 -0600

    create handleChange function and use it to set state to event's value with setValue

--- src/components/text-input/Text-input.jsx
+++ src/components/text-input/Text-input.jsx
@@ -1,10 +1,26 @@
 import './Text-input.css';
 
 function TextInput(props) {
+
+    const handleChange = (e) => {
+        props.setValue(e.target.value);
+    };
+
     return (
         <div className='input-container' role='none'>
-            <label htmlFor={props.title} className='input-container__label'>{props.title}</label>
-            <input className='input-container__input' type="text" placeholder={props.placeholder} name={props.title} id={props.title} required={props.required} />
+            <label htmlFor={props.title} className='input-container__label'>
+                {props.title}
+            </label>
+            <input 
+                className='input-container__input' 
+                type="text" 
+                value={props.value}
+                onChange={handleChange}
+                placeholder={props.placeholder} 
+                name={props.title} 
+                id={props.title} 
+                required={props.required} 
+            />
         </div>
     );
 };
