1f3714a76c9cabeb61006e5b438cc012a780390f
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-28 15:30:50 -0600

    create Team function component

---
+++ src/components/team/Team.jsx
@@ -0,0 +1,12 @@
+import './Team.css'
+
+function Team() {
+    return (
+        <section className='team' >
+            <h3 className='team__header title' >Programming</h3>
+            <div className="collaborators"></div>
+        </section>
+    );
+};
+
+export default Team
