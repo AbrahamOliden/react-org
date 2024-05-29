224eb0132531904262335ec2e636c451437ae3c3
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-28 23:47:29 -0600

    first test for member cards

--- src/components/member/Member.jsx
+++ src/components/member/Member.jsx
@@ -1,8 +1,16 @@
 import './Member.css'
 
-function Member() {
+function Member(props) {
     return (
-        <></>
+        <li className='member-card' >
+            <div className='background-accent' role='none'></div>
+            <div className='background-cream' role='none'></div>
+            <div className='info' >
+                <img src='' className='info__photo' alt='fotito' ></img>
+                <h4>Member</h4>
+                <p>Position</p>
+            </div>
+        </li>
     )
 };
 
