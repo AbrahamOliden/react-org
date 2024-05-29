d3bb6894099dfa8884ccaebb9e70bf3c834f3227
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-28 20:54:43 -0600

    create state for members and pass it it form

--- src/App.jsx
+++ src/App.jsx
@@ -8,58 +8,78 @@ import './App.css';
 function App() {
 
   const [display, setDisplay] = useState(true);
+  const [members, setMembers] = useState([]);
+  const teams = [
+      {
+        team: "Programming",
+        mainColor: '#D9F7E9',
+        accentColor: '#57C278'
+      },
+      {
+        team: 'Front End',
+        mainColor: '#E8F8FF',
+        accentColor: '#82CFFA'
+      },
+      {
+        team: 'Data Science',
+        mainColor: '#F0F8E2',
+        accentColor: '#A6D157'
+      },
+      {
+        team: 'Devops',
+        mainColor: '#FDE7E8',
+        accentColor: '#E06B69'
+      },
+      {
+        team: 'UX/UI',
+        mainColor: '#FAE9F5',
+        accentColor: '#DB6EBF'
+      },
+      {
+        team: 'Mobile',
+        mainColor: '#FFF5D9',
+        accentColor: '#FFBA05'
+      },
+      {
+        team: 'Innovation Management',
+        mainColor: '#FFEEDF',
+        accentColor: '#FF8A29'
+      }
+  ];
+
   const changeDisplay = () => {
     setDisplay( !display );
   };
 
-  const teams = [
-    {
-      team: "Programming",
-      mainColor: '#D9F7E9',
-      accentColor: '#57C278'
-    },
-    {
-      team: 'Front End',
-      mainColor: '#E8F8FF',
-      accentColor: '#82CFFA'
-    },
-    {
-      team: 'Data Science',
-      mainColor: '#F0F8E2',
-      accentColor: '#A6D157'
-    },
-    {
-      team: 'Devops',
-      mainColor: '#FDE7E8',
-      accentColor: '#E06B69'
-    },
-    {
-      team: 'UX/UI',
-      mainColor: '#FAE9F5',
-      accentColor: '#DB6EBF'
-    },
-    {
-      team: 'Mobile',
-      mainColor: '#FFF5D9',
-      accentColor: '#FFBA05'
-    },
-    {
-      team: 'Innovation Management',
-      mainColor: '#FFEEDF',
-      accentColor: '#FF8A29'
-    }
-];
+  const registerMember = (member) => {
+    console.log(members);
+    setMembers([...members, member]); // Copy array with spread operator and add new member
+  };
+
+  
 
   return (
     <>
       <Header />
-      { display && <Form teams={teams.map( (team) => team.team )} />}
+
+      { // Conditional render of Form component
+        display && 
+          <Form 
+            teams={teams.map( (team) => team.team )}
+            registerMember={registerMember}
+          />
+      };
+
       <MyOrg changeDisplay={changeDisplay} />
-      {
-        teams.map( (team) => {
-          return <Team data={team} key={team.team} />
-        })
-      }
+
+      { // Mapping Team components to teams' content
+        teams.map( (team) => <Team 
+          data={team} 
+          key={team.team} 
+          members={members} />
+        )
+      };
+
     </>
   );
 };
