ba8c4360e4215105617ef57e5024fb7f1b0beca7
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-29 17:39:33 -0600

    create conditional rendering of Team components and map their elements

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -4,14 +4,23 @@ import './Team.css';
 function Team(props) {
 
     const {team, mainColor, accentColor} = props.data;
+    const {members} = props;
 
     return (
-        <section className='team' style={{backgroundColor: mainColor}} >
-            <h3 className='team__header title' style={{borderColor: accentColor}} >{team}</h3>
-            <ul className="collaborators">
-                <Member />
-            </ul>
-        </section>
+        <>
+        {
+            members.length > 0 &&
+                <section className='team' style={{backgroundColor: mainColor}} >
+                    <h3 className='team__header title' style={{borderColor: accentColor}} >{team}</h3>
+                    <ul className="collaborators">
+                        {
+                            members.map( (member, index) => <Member data={member} key={index} /> )
+                        }
+                    </ul>
+                </section>
+        }
+        </>
+
     );
 };
 
