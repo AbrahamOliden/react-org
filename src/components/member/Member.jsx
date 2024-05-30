a25abbdec7523370e0071c1366cf1e38283aa0fd
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-29 19:53:35 -0600

    finish styling card elements

--- src/components/member/Member.jsx
+++ src/components/member/Member.jsx
@@ -6,15 +6,16 @@ function Member(props) {
     const theme = {
         backgroundColor: props.theme
     };
+    const altText = `Photo of ${name}`;
 
     return (
         <li className='member-card' >
             <div className='team-background' style={theme} role='none'></div>
             <div className='general-background' role='none'></div>
-            <div className='info' >
-                <img src={photo} className='info__photo' alt={name} ></img>
-                <h4>{name}</h4>
-                <p>{position}</p>
+            <div className='member-info' >
+                <img src={photo} className='member-info__photo' alt={altText} ></img>
+                <h4 className='member-info__name' >{name}</h4>
+                <p className='member-info__position' >{position}</p>
             </div>
         </li>
     )
