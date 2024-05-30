8f37b76773ef198a46f9273677209bb63bdbc631
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-29 21:17:23 -0600

    add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/header/Header.jsx';
 import Form from './components/form/Form.jsx';
 import MyOrg from './components/my-org/my-org.jsx';
 import Team from './components/team/Team.jsx';
+import Footer from './components/footer/Footer.jsx';
 import './App.css';
 
 function App() {
@@ -79,6 +80,8 @@ function App() {
           members={members.filter( (member) => team.team === member.team )} /> //Each component renders only the members from its team
         )
       }
+
+      <Footer />
 
     </>
   );
