6c692bc8eed6a3a5a8a2a8b0aef96c3345885a8a
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-04 19:14:34 -0600

    fix desete button behavior

--- src/components/member/Member.jsx
+++ src/components/member/Member.jsx
@@ -3,7 +3,7 @@ import { IoIosCloseCircle } from 'react-icons/io';
 
 function Member(props) {
 
-    const {name, position, photo, team} = props.data
+    const {name, position, photo, team, id} = props.data
     const {theme, deleteMember} = props
     
     const altText = `Photo of ${name}`;
@@ -11,7 +11,7 @@ function Member(props) {
     return (
         <>
         <li className='member-card' >
-            <IoIosCloseCircle className='member-card__delete' onClick={deleteMember} />
+            <IoIosCloseCircle className='member-card__delete' onClick={ () => deleteMember(id)} />
             <div className='team-background' style={{ backgroundColor: theme}} role='none'></div>
             <div className='general-background' role='none'></div>
             <div className='member-info' >
