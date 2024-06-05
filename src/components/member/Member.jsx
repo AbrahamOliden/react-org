0ab55760da563801ae47c7eb20f79ce4d6ec3692
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-05 00:30:02 -0600

    Add heart icons for favorite member feature

--- src/components/member/Member.jsx
+++ src/components/member/Member.jsx
@@ -1,9 +1,10 @@
 import './Member.css'
 import { IoIosCloseCircle } from 'react-icons/io';
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 function Member(props) {
 
-    const {name, position, photo, team, id} = props.data
+    const {name, position, photo, team, id, favorite } = props.data
     const {theme, deleteMember} = props
     
     const altText = `Photo of ${name}`;
@@ -18,6 +19,12 @@ function Member(props) {
                 <img src={photo} className='member-info__photo' alt={altText} ></img>
                 <h4 className='member-info__name' >{name}</h4>
                 <p className='member-info__position' >{position}</p>
+                {
+                    favorite
+                        ? <AiFillHeart style={{'color': 'red', 'font-size': '1.5rem'}}/>
+                        : <AiOutlineHeart style={{'font-size': '1.5rem'}} />
+                }
+                
             </div>
         </li>
         
